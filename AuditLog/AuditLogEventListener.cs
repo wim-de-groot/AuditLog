262b5ac7e627c049245404c25128bfb55333467c
wim_de_groot <wim_de_groot@icloud.com>
2019-12-07 16:20:40 +0100

    removed unnecessary try catch for logging

--- AuditLog/AuditLogEventListener.cs
+++ AuditLog/AuditLogEventListener.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
 using AuditLog.Abstractions;
 using AuditLog.Domain;
 using Microsoft.Extensions.Logging;
@@ -21,22 +22,13 @@ namespace AuditLog
         [Topic("#")]
         public void Handle(string message)
         {
-            try
-            {
-                var logEntry = JsonConvert.DeserializeObject<LogEntry>(message);
-                _logger.LogTrace($"Log entry for event: {logEntry.EventType} with routing key: {logEntry.RoutingKey} deserialized.");
+            var logEntry = JsonConvert.DeserializeObject<LogEntry>(message);
+            _logger.LogTrace(
+                $"Log entry for event: {logEntry.EventType} with routing key: {logEntry.RoutingKey} deserialized.");
 
-                _repository.Create(logEntry);
-                _logger.LogTrace($"Log entry for event: {logEntry.EventType} with routing key: {logEntry.RoutingKey} saved.");
-            }
-            catch (JsonReaderException exception)
-            {
-                _logger.LogError($"Error has occured while deserializing json, with exception: {exception.Message}");
-            }
-            catch
-            {
-                _logger.LogError($"Error has occured while saving log entry.");
-            }
+            _repository.Create(logEntry);
+            _logger.LogTrace(
+                $"Log entry for event: {logEntry.EventType} with routing key: {logEntry.RoutingKey} saved.");
         }
     }
 }
