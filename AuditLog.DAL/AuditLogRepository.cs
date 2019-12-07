2ee9973d93cb5fe05b1b8f39d65f2d52d69b331e
wim_de_groot <wim_de_groot@icloud.com>
2019-12-07 12:12:22 +0100

    Undo changes

--- AuditLog.DAL/AuditLogRepository.cs
+++ AuditLog.DAL/AuditLogRepository.cs
@@ -17,9 +17,9 @@ namespace AuditLog.DAL
 
         public IEnumerable<LogEntry> FindBy(LogEntryCriteria criteria) =>
             _context.LogEntries.Where(entry =>
-                IsNullOrGreaterOrEqualsThen(criteria.FromTimestamp, entry.Timestamp) &&
-                IsNullOrSmallerOrEqualsThen(criteria.ToTimestamp, entry.Timestamp) &&
-                IsNullOrEqualsTo(criteria.EventType, entry.EventType)
+                (criteria.FromTimestamp == null || entry.Timestamp >= criteria.FromTimestamp) &&
+                (criteria.ToTimestamp == null || entry.Timestamp <= criteria.ToTimestamp) &&
+                (criteria.EventType == null || entry.EventType == criteria.EventType)
             );
 
         public void Create(LogEntry entity)
@@ -27,14 +27,5 @@ namespace AuditLog.DAL
             _context.LogEntries.Add(entity);
             _context.SaveChanges();
         }
-
-        private static bool IsNullOrGreaterOrEqualsThen(long? criteriaTimestamp, long entryTimestamp) =>
-            criteriaTimestamp == null || entryTimestamp >= criteriaTimestamp;
-
-        private static bool IsNullOrSmallerOrEqualsThen(long? criteriaTimestamp, long entryTimestamp) =>
-            criteriaTimestamp == null || entryTimestamp <= criteriaTimestamp;
-
-        private static bool IsNullOrEqualsTo(string criteriaValue, string entryValue) =>
-            criteriaValue == null || entryValue == criteriaValue;
     }
 }
