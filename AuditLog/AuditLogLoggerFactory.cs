c03215d84b9f49033c65f8eedd7dcf306977f25d
wim_de_groot <wim_de_groot@icloud.com>
2019-12-07 16:03:01 +0100

    Changed AuditLogLoggerFactory

--- AuditLog/AuditLogLoggerFactory.cs
+++ AuditLog/AuditLogLoggerFactory.cs
@@ -8,25 +8,8 @@ namespace AuditLog
     [ExcludeFromCodeCoverage]
     public static class AuditLogLoggerFactory
     {
-        private static ILoggerFactory _loggerFactory = new NullLoggerFactory();
-        
-        [ExcludeFromCodeCoverage]
-        public static ILoggerFactory LoggerFactory
-        {
-            internal get => _loggerFactory;
-            set
-            {
-                if (_loggerFactory is NullLoggerFactory)
-                {
-                    _loggerFactory = value;
-                }
-                else
-                {
-                    throw new InvalidOperationException("Logger factory has already been set");
-                }
-            }
-        }
+        public static ILoggerFactory LoggerFactory { get; set; } = new NullLoggerFactory();
+
         public static ILogger<T> CreateInstance<T>() => LoggerFactory.CreateLogger<T>();
-        public static ILogger CreateLogger(string categoryName) => LoggerFactory.CreateLogger(categoryName);
     }
 }
