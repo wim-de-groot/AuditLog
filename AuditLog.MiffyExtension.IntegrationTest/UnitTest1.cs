06da369c713b2611afa6b2c2b59109775aac97ca
wim_de_groot <wim_de_groot@icloud.com>
2019-12-11 11:19:47 +0100

    Added integration test project

---
+++ AuditLog.MiffyExtension.IntegrationTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AuditLog.MiffyExtension.IntegrationTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
